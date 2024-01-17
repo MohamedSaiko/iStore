8d8e462dc498a02d504808c65332f52b3d10da79
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-01-17 13:47:10 +0100

    Initial Commit

---
+++ iStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 17.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iStore/iStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStoreApp.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct iStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
