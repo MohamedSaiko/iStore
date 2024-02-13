077407362c8fbc944cf5fba4e9488822de93b44f
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-02-13 12:06:32 +0100

    update category screen

--- iStore/Category/Category.swift
+++ iStore/Category/Category.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct Category: View {
+    
+    @StateObject var categoryViewModel = CategoryViewModel(networkManager: NetworkManager())
+    
     var body: some View {
         NavigationView {
-            ProductsGrid()
+            ProductsGrid(products: categoryViewModel.products)
                 .navigationTitle("Category")
                 .navigationBarTitleDisplayMode(.inline)
+        }
+        .task {
+            categoryViewModel.getProducts {
+                print(categoryViewModel.products)
+            }
         }
     }
 }
