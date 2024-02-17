65cecae404d3b9af0e99afc587971029c876cc53
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-02-17 17:53:09 +0100

    refactor ProductDetailsView
    (cherry picked from commit da6cf4e39538591246dd6bfe219834149653b47d)

--- iStore/NetworkLayer/ProductDetailsView.swift
+++ iStore/NetworkLayer/ProductDetailsView.swift
@@ -11,38 +11,110 @@ struct ProductDetailsView: View {
     
     @StateObject var productDetailsViewModel = ProductDetailsViewModel(networkManager: NetworkManager())
     
+    @State private var isFavoriteTapped = false
     var id: Int
     
     var body: some View {
-        NavigationView {
-//            if productDetailsViewModel.isloading {
-//                ProgressView()
-//            } else {
-            VStack() {
+        Group {
+            if productDetailsViewModel.isloading {
+                ProgressView()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.black)
+                    .tint(.white)
+                    .ignoresSafeArea()
+            } else {
+                VStack() {
                     ProductDetailsTabView(product: productDetailsViewModel.product)
-                        .frame(maxWidth: .infinity, maxHeight: 300)
-
-                    Text("Brand: " + productDetailsViewModel.getProductBrand())
-                        .foregroundColor(.black)
-                        .font(.system(.title, design: .rounded))
+                        .frame(height: UIScreen.main.bounds.height / 3)
+                        .shadow(color: .white, radius: 10)
                     
-                    Button {
-                        print("Image tapped!")
-                    } label: {
-                        Text("Add To Cart")
-                            .foregroundColor(.black)
-                            .font(.system(.largeTitle, design: .rounded))
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(alignment: .leading) {
+                            HStack(alignment: .center, spacing: 2) {
+                                Text(productDetailsViewModel.getProductBrand())
+                                    .foregroundColor(.black)
+                                    .font(.system(.title2, design: .rounded))
+                                
+                                Spacer()
+                                
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(.pink)
+                                
+                                Text("\(productDetailsViewModel.getProductRating(), specifier: "%.2f")")
+                                    .foregroundColor(.black)
+                                    .font(.system(.title2, design: .rounded))
+                            }
+                            
+                            Spacer()
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Desription: ")
+                                    .foregroundColor(.gray)
+                                    .font(.system(.headline, design: .rounded))
+                                
+                                Text(productDetailsViewModel.getProductDescription())
+                                    .foregroundColor(.black)
+                                    .font(.system(.headline, design: .rounded))
+                            }
+                            
+                            Spacer()
+                            Spacer()
+                            
+                            HStack() {
+                                Text("Total")
+                                    .foregroundColor(.black)
+                                    .font(.system(.title2, design: .rounded))
+                                
+                                Spacer()
+                                
+                                Text("$\(productDetailsViewModel.getProductPrice())")
+                                    .foregroundColor(.pink)
+                                    .font(.system(.title2, design: .rounded))
+                            }
+                        }
+                        .frame(height: UIScreen.main.bounds.height / 4)
+                        .padding()
+                        
+                        Button {
+                            print("Image tapped!")
+                        } label: {
+                            
+                            HStack() {
+                                Text("Add To Cart")
+                                    .foregroundColor(.white)
+                                    .font(.system(.title, design: .rounded))
+                                
+                                Spacer()
+                                
+                                Image(systemName: "bag")
+                                    .foregroundColor(.white)
+                                    .font(.system(.title, design: .rounded))
+                            }
+                            .padding(20)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding(.horizontal)
+                        .padding(.bottom)
                     }
-                    
-                    Spacer()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .ignoresSafeArea()
                 }
-                //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .background(.gray)
-            //}
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.black)
+            }
         }
-        .navigationTitle(productDetailsViewModel.product?.title ?? "")
-        .navigationBarTitleDisplayMode(.inline)
-        //.environment(\.defaultMinListHeaderHeight, 1)
+        .toolbar() {
+            Button {
+                print("favorite tapped")
+                isFavoriteTapped.toggle()
+            } label: {
+                Image(systemName: isFavoriteTapped ? "heart.fill" : "heart")
+            }
+        }
         .task {
             productDetailsViewModel.getProduct(with: id) {
                 print("success")
