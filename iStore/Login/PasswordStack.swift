678ea1ea25e913615e2800f753bc90d6e5f2551c
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 11:02:25 +0200

    add PasswordStack to LoginView

---
+++ iStore/Login/PasswordStack.swift
@@ -0,0 +1,24 @@
+//
+//  PasswordStack.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 13.03.24.
+//
+
+import SwiftUI
+
+struct PasswordStack: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text("Password")
+                .font(.subheadline)
+                .fontWeight(.bold)
+            
+            SecureField("Enter your Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+        }
+    }
+}
