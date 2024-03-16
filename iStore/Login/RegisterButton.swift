3c494e89b50291b151ebb175a1f2dc9d788e7d3b
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 11:01:22 +0200

    add RegisterButton to LoginView

---
+++ iStore/Login/RegisterButton.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterButton.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 13.03.24.
+//
+
+import SwiftUI
+
+struct RegisterButton: View {
+    var body: some View {
+        NavigationLink("Register") {
+            Text("Register")
+        }
+    }
+}
