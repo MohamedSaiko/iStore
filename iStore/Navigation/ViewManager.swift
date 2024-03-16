e2622bceadd7999b73ac93f9858f5048fa09eb4a
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 14:22:29 +0200

    add ViewManager

---
+++ iStore/Navigation/ViewManager.swift
@@ -0,0 +1,32 @@
+//
+//  ViewManager.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 16.03.24.
+//
+
+import SwiftUI
+
+struct ViewManager: View {
+    
+    @StateObject var navigationCoordinator: NavigationCoordinator
+    @StateObject var loginViewModel: LoginViewModel
+    
+    var body: some View {
+        switch navigationCoordinator.switchView {
+            case .loginView:
+                LoginView(userName: "kminchelle", password: "0lelplR")
+                    .environmentObject(navigationCoordinator)
+                    .environmentObject(loginViewModel)
+                
+            case .contentView:
+                ContentView()
+                    .environmentObject(navigationCoordinator)
+                    .environmentObject(loginViewModel)
+                
+            case .registerView:
+                Text("Register")
+                    .environmentObject(navigationCoordinator)
+        }
+    }
+}
