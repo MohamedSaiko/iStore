d43ca85cd0028ecc3c9baa269acc65627934f7ea
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 11:00:19 +0200

    add LoginView

---
+++ iStore/Login/LoginView.swift
@@ -0,0 +1,55 @@
+//
+//  LoginView.swift
+//  iStore
+//
+//  Created by Mohamed Sayed on 13.03.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var userName: String
+    @State private var password: String
+    
+    init(userName: String, password: String) {
+        self.userName = userName
+        self.password = password
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 24) {
+                Text("iStore")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding(.top, 25)
+                    .padding(.horizontal)
+                
+                Text("Get Everything at Your Doorstep")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.pink)
+                    .padding()
+                
+                Spacer()
+                
+                VStack(spacing: 24) {
+                    UserNameStack(userName: $userName)
+                    
+                    PasswordStack(password: $password)
+                }
+                .padding()
+                
+                VStack(alignment: .center, spacing: 15) {
+                    LoginButton(userName: userName, password: password)
+                    
+                    RegisterButton()
+                }
+                .padding()
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
