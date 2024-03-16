b85dd46179ac21d920c57762ff0695abb9f72370
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 11:18:28 +0200

    add LoginView as initialView

--- iStore/iStoreApp.swift
+++ iStore/iStoreApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iStoreApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView(userName: "", password: "")
         }
     }
 }
