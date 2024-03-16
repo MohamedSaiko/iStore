c0cda4343346d72e1d4fbde0b8bac66a15c4cb51
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-16 14:31:08 +0200

    add ViewManager as the Main view to iStoreApp

--- iStore/iStoreApp.swift
+++ iStore/iStoreApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iStoreApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView(userName: "kminchelle", password: "0lelplR")
+            ViewManager(navigationCoordinator: NavigationCoordinator(), loginViewModel: LoginViewModel(authenticationManager: AuthenticationManager(), currentUser: CurrentAuthenticatedUser(id: Int())))
         }
     }
 }
