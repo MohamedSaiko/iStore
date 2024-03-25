567f5a18feb0c38acc0183de38b5b26df53a4332
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-03-25 13:54:39 +0200

    add showProducts to homeViewModel

--- iStore/Home/HomeViewModel.swift
+++ iStore/Home/HomeViewModel.swift
@@ -9,14 +9,20 @@ import Foundation
 
 final class HomeViewModel: ObservableObject {
     private let networkManager: NetworkManager
+    
     @Published var products = [Product]()
+    @Published var searchText = ""
+    
+    var searchProduct: String {
+        searchText.lowercased()
+    }
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
     }
     
     func getProducts(completion: @escaping (ProductsData) -> Void) {
-        let url = allProductsUrl
+        let url = allProductsURL
         
         networkManager.loadData(withURL: url) { (result: Result<ProductsData,NetworkError>) in
             switch result {
@@ -25,6 +31,32 @@ final class HomeViewModel: ObservableObject {
                     
                 case .failure(let error):
                     print(NetworkError.unknownError(error))
+            }
+        }
+    }
+    
+    func getFilteredProducts(product: String, completion: @escaping (ProductsData) -> Void) {
+        let url = filteredProductsURL + product
+        
+        networkManager.loadData(withURL: url) { (result: Result<ProductsData,NetworkError>) in
+            switch result {
+                case .success(let data):
+                    completion(data)
+                    
+                case .failure(let error):
+                    print(NetworkError.unknownError(error))
+            }
+        }
+    }
+    
+    func showProducts(completion: @escaping (ProductsData) -> Void) {
+        if !searchText.isEmpty {
+            getFilteredProducts(product: searchProduct) { data in
+                completion(data)
+            }
+        } else {
+            getProducts() { data in
+                completion(data)
             }
         }
     }
