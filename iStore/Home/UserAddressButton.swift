68c71673dc339720cc613efbc0d6277d74f5d48a
Mohamed Sayed <mohamedsaiko9@gmail.com>
2024-04-16 08:53:32 +0200

    update userAddressButton

--- iStore/Home/UserAddressButton.swift
+++ iStore/Home/UserAddressButton.swift
@@ -12,17 +12,21 @@ struct UserAddressButton: View {
     private let postalCode: String
     private let city: String
     private let state: String
+    private let latitude: Double
+    private let longitude: Double
     
-    init(address: String, postalCode: String, city: String, state: String) {
+    init(address: String, postalCode: String, city: String, state: String, latitude: Double, longitude: Double) {
         self.address = address
         self.postalCode = postalCode
         self.city = city
         self.state = state
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
     var body: some View {
         NavigationLink {
-            LocationView()
+            LocationView(address: address, postalCode: postalCode, city: city, state: state, latitude: latitude, longitude: longitude)
         } label: {
             HStack(alignment: .center, spacing: 10) {
                 Image(systemName: "map.circle.fill")
@@ -30,7 +34,7 @@ struct UserAddressButton: View {
                     .foregroundColor(.pink)
                 
                 VStack(alignment: .leading, spacing: 5) {
-                    Text("Your Location")
+                    Text("Delivery Address")
                         .font(.system(.subheadline, design: .rounded))
                         .opacity(0.5)
                     
